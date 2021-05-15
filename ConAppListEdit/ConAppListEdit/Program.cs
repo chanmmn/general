6742c670a8eb53a7e412819a10855cea75641705
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-05-15 23:08:39 +0800

    Commit ConAppListEdit
    Commit ConAppListEdit

---
+++ ConAppListEdit/ConAppListEdit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConAppListEdit
+{
+
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+    
+    class Program
+    {
+        public static List<Products> list = new List<Products>() {
+                new Products{ProductID=1, ProductName="Chai"},
+                new Products{ProductID=3, ProductName="Aniseed Syrup"},
+                new Products{ProductID=2, ProductName="Chang"}
+         };
+
+        static void Main(string[] args)
+        {
+            foreach (Products prod in list)
+            {
+                Console.WriteLine("Product ID {0} Product Name: {1}", prod.ProductID, prod.ProductName);
+            }
+            Console.WriteLine("----------------------------");
+            UpdateList();
+            foreach (Products prod in list)
+            {
+                Console.WriteLine("Product ID {0} Product Name: {1}", prod.ProductID, prod.ProductName);
+            }
+        }
+
+        public static void UpdateList()
+        {
+            var result = from r in list where r.ProductID == 2 select r;
+            result.First().ProductName = "Chan";
+        }
+    }
+}
