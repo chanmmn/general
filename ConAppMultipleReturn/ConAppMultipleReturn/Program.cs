433aa1ca27276ec119bef8bec41366132f2aefaa
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-06-07 22:57:48 +0800

    Commit ConAppMultipleReturn
    Function returns multiple values.

---
+++ ConAppMultipleReturn/ConAppMultipleReturn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppMultipleReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int x, int y, int z) = fun();
+            Console.WriteLine("{0} {1} {2}",x,y,z);
+            Console.ReadLine();
+        }
+
+        private static (int, int, int) fun()
+        {
+            return (4, 5, 3);
+        }
+    }
+}
