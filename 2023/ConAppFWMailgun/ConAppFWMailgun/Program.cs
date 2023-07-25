c4b8cbc80170f32799ff43ba302a161b077329e6
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-07-25 02:21:02 -0700

    Commit Mailgun changes
    Commit Mailgun changes

--- 2023/ConAppFWMailgun/ConAppFWMailgun/Program.cs
+++ 2023/ConAppFWMailgun/ConAppFWMailgun/Program.cs
@@ -16,14 +16,12 @@ namespace ConAppFWMailgun
             {
                 var authToken = Encoding.ASCII.GetBytes($"api:privatekey");
 
-                //var authToken = Encoding.ASCII.GetBytes($"api:49a3f5df019c65abd5ac23482bff84e0 - 4de08e90 - d211f48d");
-
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
 
                 var formContent = new FormUrlEncodedContent(new Dictionary<string, string> {
-        { "from", $"norep1y@mail.fathopesenergy.com" },
-        { "h:Reply-To", $"norep1y@mail.fathopesenergy.com" },
-        { "to", "azeem@fathopesenergy.com, internettoo@fathopesenergy.com" },
+        { "from", $"norep1y@mail.domain.com" },
+        { "h:Reply-To", $"norep1y@mail.domain.com" },
+        { "to", "user@domain.com" },
         { "subject", "Test Mailgun with two recipients" },
         { "text", "Test Sent by Mailgun" },
         { "html", "Test Sent by Mailgun" }
