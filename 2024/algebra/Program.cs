be3dd4986a096d1b408c61b18bcfd4976c3d76c0
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-03-12 00:36:51 +0800

    Commit Algebra
    Commit Algebra

---
+++ 2024/algebra/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal Price = 0;
+        Console.WriteLine("Hello, Algebra");
+        Price = CalculateCharge(20,4,7);
+        Console.WriteLine(Price);
+    }
+
+    static decimal CalculateCharge(int BaseCharge, int PerItemCharge, int Window)
+        {
+            //const decimal BaseCharge = 20.0m;
+            //const decimal PerItemCharge = 4.0m;
+
+            decimal totalCharge = BaseCharge + (PerItemCharge * Window);
+            return totalCharge;
+        }
+}
