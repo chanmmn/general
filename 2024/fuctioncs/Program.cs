afa6e3b4ee7281addf406dea2a0d32be6f1d7bdd
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-03-21 18:02:13 +0800

    Commit functioncs
    Commit functioncs

---
+++ 2024/fuctioncs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EquationSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage: Solve 2b = 6
+            double b = SolveEquation(2, 6);
+            Console.WriteLine($"The value of b is: {b}");
+        }
+
+        static double SolveEquation(double coefficient, double constant)
+        {
+            // Solve for b: 2b = 6
+            // Divide both sides by 2
+            double b = constant / coefficient;
+            return b;
+        }
+    }
+}
