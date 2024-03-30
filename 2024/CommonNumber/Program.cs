8265118a1d0048bbb7efdc3b9c9a2fc1535d21c8
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-03-30 06:23:53 -0700

    commit  Least Common Multiple
    commit  Least Common Multiple

---
+++ 2024/CommonNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class LCMCalculator
+{
+    // Function to find the greatest common factor (GCF) using Euclid's Algorithm
+    static int GCF(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Function to calculate the least common multiple (LCM)
+    static int LCM(int a, int b)
+    {
+        // LCM = (a * b) / GCF(a, b)
+        return (a / GCF(a, b)) * b;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two numbers to find their LCM:");
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        int result = LCM(num1, num2);
+        Console.WriteLine($"LCM of {num1} and {num2} is {result}");
+
+        Console.ReadLine();
+    }
+}
