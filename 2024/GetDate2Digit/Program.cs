3ab416881d80ec4af5aea93c4e7a1f4689081634
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-04-20 12:35:50 +0800

    Commit GetDate2Digit
    Commit GetDate2Digit

---
+++ 2024/GetDate2Digit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ExtractDate();
+            // Example date
+
+        }
+
+        public static void ExtractDate()
+        {
+            DateTime date = DateTime.Now;
+
+            // Extracting 2 digits for day, month, and year
+            string day = date.ToString("dd");
+            string month = date.ToString("MM");
+            string year = date.ToString("yy");
+
+            // Printing the extracted values
+            Console.WriteLine($"Day: {day}");
+            Console.WriteLine($"Month: {month}");
+            Console.WriteLine($"Year: {year}");
+    }
+}
+}
+
+
+
+
