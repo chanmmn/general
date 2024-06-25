a4747583f68bc5835180be7c12fbfb18b9bfd5b2
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-06-25 09:22:39 -0700

    Commit DependencyInjection
    Commit DependencyInjection

---
+++ 2024/DependencyInjection/ConAppDI/ConAppDI/PlaystationConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppNoDI
+{
+    internal class PlaystationConsole
+    {
+        
+        GameLoader gameLoader = new GameLoader();
+        public PlaystationConsole(GameLoader gameLoaderr)
+        {
+            this.gameLoader = gameLoaderr;
+        }
+
+        public void Play()
+        {
+            gameLoader.Load();
+        }
+    }
+}
---
+++ 2024/DependencyInjection/ConAppDI/ConAppDI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConAppNoDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            PlaystationConsole playstationConsole = new PlaystationConsole(new GameLoader("God of War"));
+            playstationConsole.Play();
+        }
+    }
+}
