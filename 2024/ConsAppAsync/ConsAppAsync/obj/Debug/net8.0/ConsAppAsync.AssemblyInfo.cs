767119b672f9c8f91553344d5b180f8a740a459a
Ming Man <chanmmn@hotmail.com>
2024-10-27 22:58:33 +0800

    Commit update
    Commit update

--- 2024/ConsAppAsync/ConsAppAsync/obj/Debug/net8.0/ConsAppAsync.AssemblyInfo.cs
+++ 2024/ConsAppAsync/ConsAppAsync/obj/Debug/net8.0/ConsAppAsync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsAppAsync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd6ccbe6abdce4a4c84d2dbb5d1f14310b4f4f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsAppAsync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsAppAsync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
