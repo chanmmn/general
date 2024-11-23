c10da93bf12982181b122a16675480a3b4c0b797
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-11-23 17:11:08 +0800

    Commit ConAppExtractDateofMonth
    Commit ConAppExtractDateofMonth

---
+++ 2024/ConAppExtractDateofMonth/ConAppExtractDateofMonth/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConAppExtractDateofMonth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dates = GetBeingAndEndDateOfTheMonth();
+            Console.WriteLine($"Start Date: {dates.Item1}");
+            Console.WriteLine($"End Date: {dates.Item2}");
+        }
+
+        public static Tuple<string, string> GetBeingAndEndDateOfTheMonth()
+        {
+            DateTime today = DateTime.Today;
+            DateTime startDate = new DateTime(today.Year, today.Month, 1);
+            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            return Tuple.Create(startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"));
+        }
+    }
+}
