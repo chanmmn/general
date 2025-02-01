d16209d9be19388eede38466604f1581ff3b8d94
chanmmn <33271651+chanmmn@users.noreply.github.com>
2025-02-02 00:09:01 +0800

    Cimmit GetNextMonthFirstDay

---
+++ 2025/GetNextMonthFirstDay/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(ChangeFormat());
+    }
+
+       private static string ChangeFormat()
+    {
+        DateTime FirstDayNextMonth = GetFirstDayOfNextMonth();
+        Console.WriteLine("Last day of last month is: " + FirstDayNextMonth.ToString("yyyy-MM-dd HH:mm:ss"));
+        return FirstDayNextMonth.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+
+    private static DateTime GetFirstDayOfNextMonth()
+    {
+        DateTime today = DateTime.Today;
+        DateTime firstDayOfNextMonth = new DateTime(today.Year, today.Month, 1).AddMonths(1);
+        return firstDayOfNextMonth;
+    }
+
+}
