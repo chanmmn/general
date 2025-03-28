5d8044e6972a61360ef7473529ebc2b2bd6edfc2
chanmmn <33271651+chanmmn@users.noreply.github.com>
2025-03-28 08:17:04 -0700

    Commit ConAppDecimalBinary

---
+++ 2025/ConAppDecimalBinary/ConAppDecimalBinary/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConAppDecimalBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            float decimalNumber = 43.23f
+                ;
+            string binaryNumber2 = DecimalToBinary(decimalNumber);
+            Console.WriteLine($"Decimal: {decimalNumber}, Binary: {binaryNumber2}");
+        }
+
+        private static string DecimalToBinary(float decimalNumber)
+        {
+            int intPart = (int)decimalNumber;
+            float floatPart = decimalNumber - intPart;
+            string binaryIntPart = "";
+            string binaryFloatPart = "";
+            while (intPart > 0)
+            {
+                binaryIntPart = (intPart % 2) + binaryIntPart;
+                intPart /= 2;
+            }
+            while (floatPart > 0)
+            {
+                floatPart *= 2;
+                binaryFloatPart += (int)floatPart;
+                floatPart -= (int)floatPart;
+            }
+            return binaryIntPart + "." + binaryFloatPart;
+        }
+
+    }
+}
