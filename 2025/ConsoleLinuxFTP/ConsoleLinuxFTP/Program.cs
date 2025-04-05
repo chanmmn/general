30f22a6ce910a7281318ea7b13941da9e220b799
Administrator <mingitdynasty@outlook.com>
2025-04-05 03:37:40 -0700

    Commit ConsoleLinuxFTP
    Commit ConsoleLinuxFTP

---
+++ 2025/ConsoleLinuxFTP/ConsoleLinuxFTP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace ConsoleLinuxFTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TransferFileToFTP("HelloFtp.txt");
+        }
+
+        static void TransferFileToFTP(string filePath)
+        {
+            string ftpServer = "ftp://172.23.143.141";
+            string username = "user1";
+            string password = "password";
+            string ftpFilePath = $"{ftpServer}/{Path.GetFileName(filePath)}";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpFilePath);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(username, password);
+            byte[] fileContents;
+            using (StreamReader sourceStream = new StreamReader(filePath))
+            {
+                fileContents = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            }
+            request.ContentLength = fileContents.Length;
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(fileContents, 0, fileContents.Length);
+            }
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                Console.WriteLine($"Upload File Complete, status {response.StatusDescription}");
+            }
+        }
+    }
+}
